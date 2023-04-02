a766fbefbd73936102192c6aef64dab0e14e288d
smaran-m <77597185+smaran-m@users.noreply.github.com>
2023-04-01 21:18:31 -0400

    add click and drag and a (not working) save button

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,21 +1,26 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
-using Avalonia.VisualTree;
 
 namespace GridShadingApp;
 
 public partial class MainWindow : Window
 {
-    private const int numRows = 20;
-    private const int numCols = 20;
+    private const int numRows = 18;
+    private const int numCols = 32;
     private double cellSize;
-    private SolidColorBrush defaultBrush = new SolidColorBrush(Colors.White);
-    //private SolidColorBrush shadedBrush = new SolidColorBrush(Colors.Black);
+    //private SolidColorBrush defaultBrush = new SolidColorBrush(Colors.White);
+    private SolidColorBrush defaultBrush = new SolidColorBrush(Colors.Transparent);
+    private SolidColorBrush shadedBrush = new SolidColorBrush(Colors.Black);
     private ImageBrush imageBrush = new ImageBrush(new Bitmap("Images/test.png"));
+    private HashSet<Border> modifiedCells = new HashSet<Border>();
+
 
 
     public MainWindow()
@@ -23,8 +28,8 @@ public partial class MainWindow : Window
         InitializeComponent();
         //this.CanResize = false;
         this.Opened += MainWindow_Opened;
-        this.LayoutUpdated += MainWindow_LayoutUpdated;
-        this.PointerMoved += MainWindow_PointerMoved;
+        mainGrid.PointerMoved += MainGrid_PointerMoved;
+        saveButton.Click += SaveButton_Click;
 
         // Event Handlers
         void MainWindow_Opened(object sender, EventArgs e)
@@ -34,7 +39,7 @@ public partial class MainWindow : Window
             CreateGrid();
         }
 
-        void MainWindow_LayoutUpdated(object? sender, EventArgs e)
+        /*void MainWindow_LayoutUpdated(object? sender, EventArgs e)
         {
             var windowSize = this.ClientSize;
             double aspectRatio = (double)numCols / numRows;
@@ -62,23 +67,7 @@ public partial class MainWindow : Window
                     cell.Height = cellSize;
                 }
             }
-        }
-
-        void MainWindow_PointerMoved(object? sender, PointerEventArgs e)
-        {
-            if (e.GetCurrentPoint(this).Properties.PointerUpdateKind == PointerUpdateKind.LeftButtonPressed)
-            {
-                var point = e.GetPosition(mainGrid);
-                int col = (int)(point.X / cellSize);
-                int row = (int)(point.Y / cellSize);
-
-                if (col >= 0 && col < numCols && row >= 0 && row < numRows)
-                {
-                    Border cell = (Border)mainGrid.Children[row * numCols + col];
-                    cell.Background = imageBrush;
-                }
-            }
-        }
+        }*/
     }
 
     private void CreateGrid()
@@ -100,13 +89,14 @@ public partial class MainWindow : Window
                 Border cell = new Border
                 {
                     BorderThickness = new Thickness(1),
-                    BorderBrush = new SolidColorBrush(Colors.Black),
+                    BorderBrush = new SolidColorBrush(Colors.LightGray),
                     Background = defaultBrush,
                     Width = cellSize,
                     Height = cellSize,
                 };
 
                 cell.PointerPressed += Cell_PointerPressed;
+                cell.PointerReleased += Cell_PointerReleased;
 
                 Grid.SetRow(cell, i);
                 Grid.SetColumn(cell, j);
@@ -115,12 +105,70 @@ public partial class MainWindow : Window
         }
     }
 
-    private void Cell_PointerPressed(object sender, PointerPressedEventArgs e)
+    private bool isDragging = false;
+
+    private void Cell_PointerPressed(object? sender, PointerPressedEventArgs e)
     {
         if (sender is Border cell)
         {
-            cell.Background = cell.Background == defaultBrush ? imageBrush : defaultBrush;
+            isDragging = true;
+            cell.Background = cell.Background == shadedBrush ? defaultBrush : shadedBrush;
+            modifiedCells.Add(cell);
         }
+    }
+
+    private void MainGrid_PointerMoved(object? sender, PointerEventArgs e)
+    {
+        if (isDragging)
+        {
+            var point = e.GetPosition(mainGrid);
+            int row = (int)(point.Y / cellSize);
+            int col = (int)(point.X / cellSize);
+
+            if (row >= 0 && row < numRows && col >= 0 && col < numCols)
+            {
+                Border cell = mainGrid.Children[row * numCols + col] as Border;
+                if (cell != null && !modifiedCells.Contains(cell))
+                {
+                    // Toggle the cell background between shadedBrush and defaultBrush
+                    cell.Background = cell.Background == shadedBrush ? defaultBrush : shadedBrush;
+                    modifiedCells.Add(cell);
+                }
+            }
+        }
+    }
+
+    private void Cell_PointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        isDragging = false;
+        modifiedCells.Clear(); // Clear the modified cells set
+    }
+
+    private void SaveButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        SaveGridState();
+    }
+
+    private void SaveGridState()
+    {
+        // You can use the modifiedCells set to save the state of the grid.
+        // For example, you can store the row and column information and the brush color.
+        List<(int row, int col, string color)> gridState = new List<(int row, int col, string color)>();
+
+        foreach (Border cell in modifiedCells)
+        {
+            int row = Grid.GetRow(cell);
+            int col = Grid.GetColumn(cell);
+            string color = cell.Background == shadedBrush ? "shaded" : "default";
+
+            gridState.Add((row, col, color));
+        }
+
+        // Save the gridState to a file or any other storage you prefer.
+        // For example, you can serialize the gridState list and save it to a file.
+        string json = JsonSerializer.Serialize(gridState);
+        Console.WriteLine(gridState);
+        File.WriteAllText("gridState.json", json);
     }
 
 }
